6d668b24823d7d268dc5a5d0e9756e0a8b0fc768
David Bertani <dbertani@bwl.com.mx>
2020-04-30 13:51:13 -0500

    bluee v1.0.00

--- Bluee/src/Bluee.h
+++ Bluee/src/Bluee.h
@@ -8,9 +8,9 @@
 #define _Bluee_h
 
 #if defined(ARDUINO) && ARDUINO >= 100
-	#include <arduino.h>
+	#include "arduino.h"
 #else
-	#include <WProgram.h>
+	#include "WProgram.h"
 #endif
 //#define DEBUG
 
